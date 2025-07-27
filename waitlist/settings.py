d05d986bd49e4adbb7070d2d8a8defd4f470fb98
Adeyemi Adedeji <adeyemiadedeji859@gmail.com>
2025-07-27 18:55:56 +0100

    GO

--- waitlist/settings.py
+++ waitlist/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='').split()
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
 
 
 # Application definition
